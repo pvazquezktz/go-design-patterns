c149083fc53f029643d488ea76d4d413788e668c
Pedro Vazquez <pedro.vazquez@mercadolibre.com>
2020-05-25 15:03:22 -0300

    Updates

---
+++ precache-manifest.4c5d7eac6da999cf501ac05c9109d8b9.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c44fb88218375f42fdd4fa193c7c8c7",
+    "url": "/go-design-patterns/index.html"
+  },
+  {
+    "revision": "7010ef6684cd782f6c94",
+    "url": "/go-design-patterns/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e7e090659fa454e7a80f",
+    "url": "/go-design-patterns/static/js/2.12032f5a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/go-design-patterns/static/js/2.12032f5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7010ef6684cd782f6c94",
+    "url": "/go-design-patterns/static/js/main.49072930.chunk.js"
+  },
+  {
+    "revision": "bdb657f913603f62fe57",
+    "url": "/go-design-patterns/static/js/runtime-main.14eaf2d9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/go-design-patterns/static/media/logo.5d5d9eef.svg"
+  }
+]);
