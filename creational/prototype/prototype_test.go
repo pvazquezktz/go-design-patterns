432ee837b6bb22cb9499fcae29e7c3b8c2c7c746
Pedro Vazquez <pedro.vazquez@mercadolibre.com>
2020-06-17 14:45:25 -0300

    Fix test from Prototype example

--- creational/prototype/prototype_test.go
+++ creational/prototype/prototype_test.go
@@ -10,9 +10,18 @@ func TestGetVeggiePizza(t *testing.T) {
 	pizzaCloner := PizzaCloner{}
 
 	pVeggie, err := pizzaCloner.GetClone(Veggie)
+	pVeggie2, err2 := pizzaCloner.GetClone(Veggie)
+
 	assert.Nil(t, err, "err should be nil")
+	assert.Nil(t, err2, "err2 should be nil")
+
+	// Not the same instance
+	assert.True(t, pVeggie != pVeggie2, "pVeggie should not be equal to pVeggie2")
+
 	msg := pVeggie.GetInfo()
+	msg2 := pVeggie2.GetInfo()
 	assert.Equal(t, "Contains: Tomato sauce, Cheese, Eggplant, Mushroom - Shape: Circle - Dough: Gluten Free", msg, "msg is not the expected")
+	assert.Equal(t, "Contains: Tomato sauce, Cheese, Eggplant, Mushroom - Shape: Circle - Dough: Gluten Free", msg2, "msg2 is not the expected")
 
 }
 
@@ -20,10 +29,19 @@ func TestGetSpecialPizza(t *testing.T) {
 
 	pizzaCloner := PizzaCloner{}
 
-	sVeggie, err := pizzaCloner.GetClone(Special)
+	pSpecial, err := pizzaCloner.GetClone(Special)
+	pSpecial2, err2 := pizzaCloner.GetClone(Special)
+
 	assert.Nil(t, err, "err should be nil")
-	msg := sVeggie.GetInfo()
+	assert.Nil(t, err2, "err2 should be nil")
+
+	// Not the same instance
+	assert.True(t, pSpecial != pSpecial2, "pSpecial should not be equal to pSpecial2")
+
+	msg := pSpecial.GetInfo()
+	msg2 := pSpecial2.GetInfo()
 	assert.Equal(t, "Contains: Tomato sauce, Cheese, Ham, Pepper - Shape: Square - Dough: Regular", msg, "msg is not the expected")
+	assert.Equal(t, "Contains: Tomato sauce, Cheese, Ham, Pepper - Shape: Square - Dough: Regular", msg2, "msg2 is not the expected")
 
 }
 
